bac2f9cab7e87749d31e78d862be801b4ab35d44
Viacheslav Kostevych <viacheslav.o.kostevych@raiffeisen.ua>
2022-11-29 00:15:06 +0200

    fix log format

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#### Pingator v.3.4
+#### Pingator v.3.5
 from prometheus_client import start_http_server, Gauge
 from time import sleep as sleep
+from datetime import datetime
 import ping3
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -14,14 +15,18 @@ server_port = 8000
 PING_TIME = Gauge('ping_time_host', 'Return time ping to host',['ping_host'])
 GET_RESPONCE = Gauge('responce_from_url', 'Responce from url',['url'])
 
-def ping_gauge(h):
+def get_date_time():
+    now = datetime.now()  # current date and time
+    date_time = now.strftime("%d.%m.%Y %H:%M:%S")
+    return(date_time)
+def ping_gauge(host):
     try:
-      result = ping3.ping(h, unit='ms')
+      result = ping3.ping(host, unit='ms')
     except ping3.errors.HostUnknown:  # Specific error is catched.
-        print(tab+h+':'+"Host unknown error raised.")
+        print(tab+get_date_time()+' '+host+' '+"Host unknown error raised.")
         result = 9999
     except ping3.errors.PingError:  # All ping3 errors are subclasses of `PingError`.
-        print(tab+h+':'+"A ping error raised.")
+        print(tab+get_date_time()+' '+host+' '+"A ping error raised.")
         result = 9999
     return(result)
 
@@ -34,7 +39,7 @@ def get_ping_time():
 
     for host in hosts:
         result = ping_gauge(host)
-        print(tab+host +':'+str(result))
+        print(tab+get_date_time()+' '+ host +' '+str(result))
         PING_TIME.labels(host).set(result)
 
 def get_url_responce(url):
@@ -43,13 +48,13 @@ def get_url_responce(url):
         responce_code = responce.status_code
         responce.raise_for_status()
     except requests.exceptions.HTTPError as errh:
-        print(tab + url + ' '  +":Http Error:", errh)
+        print(tab +get_date_time()+' '+url + ' '  +" :Http Error:", errh)
     except requests.exceptions.ConnectionError as errc:
-        print(tab + url + ' '  + " :Error Connecting:", errc)
+        print(tab +get_date_time()+' '+url + ' '  + " :Error Connecting:", errc)
     except requests.exceptions.Timeout as errt:
-        print(tab + url + ' '  + " :Timeout Error:", errt)
+        print(tab +get_date_time()+' '+url + ' '  + " :Timeout Error:", errt)
     except requests.exceptions.RequestException as err:
-        print(tab + url + ' '  + " :OOps: Something Else", err)
+        print(tab +get_date_time()+' '+url + ' '  + " :OOps: Something Else", err)
 
     return(responce_code)
 
@@ -62,7 +67,7 @@ def get_urls_responces():
 
     for url in urls:
         result = get_url_responce(url)
-        print(tab +url +' :' +str(result))
+        print(tab+get_date_time()+' ' +url +' ' +str(result))
         GET_RESPONCE.labels(url).set(result)
 
 if __name__ == '__main__':
